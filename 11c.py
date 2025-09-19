5020b8e9d634517115088118bfb5fc120e0ea5a8
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-19 21:39:24 +0530

    Add files via upload

---
+++ 11c.py
@@ -0,0 +1,16 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+print("The elements in the list :",my_list) 
+first_five_elements = my_list[:5] 
+print("First 5 elements:", first_five_elements) 
+ast_five_elements = my_list[-5:] 
+rint("Last 5 elements:", last_five_elements) 
+all_except_first_two = my_list[2:] 
+print("All elements except the first 2:", all_except_first_two) 
+ll_except_last_two = my_list[:-2] 
+print("All elements except the last 2:", all_except_last_two) 
+elements_2_to_7 = my_list[2:7] 
+print("Elements from index 2 to 7:", elements_2_to_7) 
+reversed_list = my_list[::-1] 
+print("Reversed list:", reversed_list) 
+sublist_step_3 = my_list[::3] 
+print("Sublist with a step of 3:", sublist_step_3)
