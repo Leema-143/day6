23e44abdf09c5384b43e601a9fa68054e18f585e
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-19 21:40:25 +0530

    Add files via upload

---
+++ 11b.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry"] 
+print("The elements in the list are: \n ", thislist) 
+thislist.insert(1, "orange") 
+print("After performing insertion operation the elements are :\n",thislist)
