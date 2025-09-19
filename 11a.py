84d5cab0127893b25814871e7eed8ccbb6aed2f5
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-19 21:41:14 +0530

    Add files via upload

---
+++ 11a.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry"] 
+print("The elements in the list are: \n ", thislist) 
+thislist.append("orange") 
+print("After performing append operation the elements are :\n", thislist)
