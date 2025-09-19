efe5246cf72536a4111018413168e65522c4aac9
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-19 21:42:11 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,8 @@
+string = input("Enter string: ") 
+count = 0 
+for character in string: 
+    # Increment the count for each character 
+    count += 1 
+# Print the length of the string 
+print("Length of the string is:") 
+print(count)
